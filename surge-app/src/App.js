593fc7edc0c1a94e0c7c00e4875a140fe9d881fc
Grace Luo <grace.r.luo@gmail.com>
2020-02-24 21:43:07 -0800

    about us page progress

--- surge-app/src/App.js
+++ surge-app/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Projects from "./components/Projects";
+import About from "./components/About";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <About></About>
         <Projects></Projects>
       </div>
     );
