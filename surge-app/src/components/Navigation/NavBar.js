d4d727881042cffa5b5993fc6b8138b75349a32a
Kash Khodabakhshi <kashk94@gmail.com>
2020-02-24 23:03:53 -0800

    Navbar works!

---
+++ surge-app/src/components/Navigation/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import SurgeLogo from "../../assets/surge-logo-small.png"
+import './NavBar.css';
+
+const NavBar = props => (
+    <header className="navbar">
+        <nav className="navbar-navigator">
+            <div></div>
+            <div className="navbar-logo">
+                <a href="/">
+                    <img src={SurgeLogo} alt="Surge Logo"></img>
+                </a>
+            </div>
+            <div className="navbar-items-spacer"></div>
+            <div className="navbar-items">
+                <ul>
+                    <li><a href="/">About</a></li>
+                    <li><a href="/">ProjectHub</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default NavBar;
