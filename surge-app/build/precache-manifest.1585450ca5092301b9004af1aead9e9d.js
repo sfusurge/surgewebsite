75f4959f9e8184192263a2e92d7b6bebca4df591
Rauf Shimarov <shimarov00@gmail.com>
2020-03-03 16:28:37 -0800

    build

---
+++ surge-app/build/precache-manifest.1585450ca5092301b9004af1aead9e9d.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d77cc088fdc8e21f9d92d6d383f0e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39703c8510d09ad51be0",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "53871f7b39e1cac226a8",
+    "url": "/static/css/main.88721a30.chunk.css"
+  },
+  {
+    "revision": "39703c8510d09ad51be0",
+    "url": "/static/js/2.8f55bd95.chunk.js"
+  },
+  {
+    "revision": "76bbb5dbcf32f9e8b66fee98c30112db",
+    "url": "/static/js/2.8f55bd95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53871f7b39e1cac226a8",
+    "url": "/static/js/main.0213da4d.chunk.js"
+  },
+  {
+    "revision": "8850ebdbdd0475470cf1",
+    "url": "/static/js/runtime-main.af13d9da.js"
+  },
+  {
+    "revision": "babbbcb67df68d3383fe5352b457ac9d",
+    "url": "/static/media/ABOUT US.babbbcb6.png"
+  },
+  {
+    "revision": "32a2bc0448d23bbcc4c72cf9daf4451e",
+    "url": "/static/media/PROJECTSTITLE.32a2bc04.png"
+  },
+  {
+    "revision": "4105ba35a34fdcdd5ca94fe2f1a6cf06",
+    "url": "/static/media/SURGETITLE.4105ba35.png"
+  },
+  {
+    "revision": "b5c09919c191d4b6ba98acb06e813ecb",
+    "url": "/static/media/aboutHeader.b5c09919.png"
+  },
+  {
+    "revision": "855be1298059d20408475ae316343da5",
+    "url": "/static/media/ali.855be129.JPG"
+  },
+  {
+    "revision": "b2450ac397382905c3ef1bba01d7d828",
+    "url": "/static/media/art.b2450ac3.JPG"
+  },
+  {
+    "revision": "eb3e3f56eb8d0d08eeef13e019f49c5c",
+    "url": "/static/media/emma.eb3e3f56.JPG"
+  },
+  {
+    "revision": "243cd3494ba860ee42f5c7225b8732d3",
+    "url": "/static/media/eventImg1.243cd349.png"
+  },
+  {
+    "revision": "6fa0d87080ef3615ffadaa0899f74b3f",
+    "url": "/static/media/eventImg2.6fa0d870.png"
+  },
+  {
+    "revision": "7aac104e8b818fea5cfc95b755534ba7",
+    "url": "/static/media/fiona.7aac104e.JPG"
+  },
+  {
+    "revision": "52c268b12a8b189f5e9eb9f9198dd1a7",
+    "url": "/static/media/hilal.52c268b1.JPG"
+  },
+  {
+    "revision": "2c72be7cb3a6496096583e9108a421e8",
+    "url": "/static/media/ishaan.2c72be7c.JPG"
+  },
+  {
+    "revision": "e581c1f4c01d8ec5bc418264806ff338",
+    "url": "/static/media/jeffrey.e581c1f4.JPG"
+  },
+  {
+    "revision": "4d13e8e66388645f1ca631006ac1fae0",
+    "url": "/static/media/joseph.4d13e8e6.JPG"
+  },
+  {
+    "revision": "3763b05804e4914243acb7183beaaf43",
+    "url": "/static/media/julia.3763b058.JPG"
+  },
+  {
+    "revision": "7bd43a9d1957111d77595cf9d10f8573",
+    "url": "/static/media/kim.7bd43a9d.JPG"
+  },
+  {
+    "revision": "b361bdf72710dd2a417235d595964cb5",
+    "url": "/static/media/michelle.b361bdf7.JPG"
+  },
+  {
+    "revision": "9df3ce3a4a0e625162422e6483aedd56",
+    "url": "/static/media/naomi.9df3ce3a.JPG"
+  },
+  {
+    "revision": "f60a0c30d7502d9e6efd365e13abefed",
+    "url": "/static/media/noble.f60a0c30.JPG"
+  },
+  {
+    "revision": "51af990c3c9b82078331c9b36ce5cd89",
+    "url": "/static/media/praneer.51af990c.JPG"
+  },
+  {
+    "revision": "f4c566433188a9ca4249a3dc68a311bd",
+    "url": "/static/media/projectsHeader.f4c56643.png"
+  },
+  {
+    "revision": "4824612cbd9c6740e48544e127b287f3",
+    "url": "/static/media/vivian.4824612c.JPG"
+  },
+  {
+    "revision": "d42edf93e1565d38a9dd624bd35fd795",
+    "url": "/static/media/whatWeDoImg1.d42edf93.png"
+  },
+  {
+    "revision": "2e96e2dba2371461988096ad86c98873",
+    "url": "/static/media/whatWeDoImg2.2e96e2db.png"
+  },
+  {
+    "revision": "ec2363c7ab6995f663e8ed3a76e8da63",
+    "url": "/static/media/whatWeDoImg3.ec2363c7.png"
+  },
+  {
+    "revision": "70ea65d655cb16d6220010d98723ae3a",
+    "url": "/static/media/whoAreWe.70ea65d6.png"
+  }
+]);
